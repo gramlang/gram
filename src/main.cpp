bf63ccee34b650d194f563369c455d2a18689b8e
Stephan Boyer <stephan@stephanboyer.com>
2016-03-30 00:58:20 -0700

    Change demo to not use /dev/stdout because clang doesn't support it

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 int main() {
-  cout << llc("/dev/stdout", "define i64 @main() {\n  ret i64 0\n}\n") << endl;
+  cout << llc("output", "define i64 @main() {\n  ret i64 0\n}\n") << endl;
   return 0;
 }
