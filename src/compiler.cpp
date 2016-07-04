3e40d500a5dcdf7a60e1415ca02b2ec15bb6b274
Stephan Boyer <stephan@stephanboyer.com>
2016-07-03 17:57:26 -0700

    Simplify some LLVM code and make it thread-safe

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -7,20 +7,15 @@
 
 void gram::compile(std::string input_path, std::string output_path) {
   try {
-    llvm::Module module("test", llvm::getGlobalContext());
-    llvm::Function* main_fn = llvm::cast<llvm::Function>(
-      module.getOrInsertFunction(
-        "main",
-        llvm::IntegerType::getInt64Ty(llvm::getGlobalContext()),
-        NULL
-      )
+    llvm::LLVMContext context;
+    llvm::Module module("test", context);
+    auto main_fn_type = llvm::FunctionType::get(llvm::Type::getInt64Ty(context), false);
+    auto main_fn = llvm::Function::Create(main_fn_type,
+      llvm::Function::ExternalLinkage, "main", &module);
+    auto basic_block = llvm::BasicBlock::Create(context, "", main_fn);
+    basic_block->getInstList().push_back(
+      llvm::ReturnInst::Create(context, llvm::ConstantInt::get(llvm::Type::getInt64Ty(context), 0))
     );
-    llvm::BasicBlock* block = llvm::BasicBlock::Create(llvm::getGlobalContext(), "", main_fn);
-    llvm::IRBuilder<> builder(block);
-    builder.CreateRet(
-      llvm::ConstantInt::getSigned(llvm::IntegerType::getInt64Ty(llvm::getGlobalContext()), 0)
-    );
-
     llc(output_path, module);
   } catch(std::runtime_error &e) {
     throw error(e.what());
