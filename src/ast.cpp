32169d0017b108b68a176036e14b5c60d5a41234
Stephan Boyer <stephan@stephanboyer.com>
2016-07-04 22:25:29 -0700

    Add basic AST

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,1 +1,28 @@
 #include "ast.h"
+
+  gram::Term::Term() {
+  }
+
+  gram::Abstraction::Abstraction(
+    std::string argument_name,
+    gram::Term *argument_type,
+    gram::Term *body) :
+    argument_name(argument_name),
+    argument_type(argument_type),
+    body(body) {
+  }
+
+  gram::Variable::Variable(std::string name, gram::Abstraction *abstraction) :
+    name(name), abstraction(abstraction) {
+  }
+
+  gram::Application::Application(gram::Term *abstraction, gram::Term *argument) :
+    abstraction(abstraction), argument(argument) {
+  }
+
+  gram::PiType::PiType(std::string argument_name, gram::Term *argument_type, gram::Term *body) :
+    argument_name(argument_name), argument_type(argument_type), body(body) {
+  }
+
+  gram::Type::Type() {
+  }
--- src/ast.h
+++ src/ast.h
@@ -5,7 +5,49 @@
 #ifndef GRAM_AST_H
 #define GRAM_AST_H
 
+#include <string>
+
 namespace gram {
+
+  class Term {
+  protected:
+    Term();
+  };
+
+  class Abstraction : public Term {
+  public:
+    std::string argument_name;
+    gram::Term *argument_type;
+    gram::Term *body;
+    Abstraction(std::string argument_name, gram::Term *argument_type, gram::Term *body);
+  };
+
+  class Variable : public Term {
+  public:
+    std::string name;
+    gram::Abstraction *abstraction;
+    Variable(std::string name, gram::Abstraction *abstraction);
+  };
+
+  class Application : public Term {
+  public:
+    gram::Term *abstraction;
+    gram::Term *argument;
+    Application(gram::Term *abstraction, gram::Term *argument);
+  };
+
+  class PiType : public Term {
+  public:
+    std::string argument_name;
+    gram::Term *argument_type;
+    gram::Term *body;
+    PiType(std::string argument_name, gram::Term *argument_type, gram::Term *body);
+  };
+
+  class Type : public Term {
+  public:
+    Type();
+  };
 
 }
 
