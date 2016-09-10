9b96b8059ba8bfa3b465beb971e270ae006aeade
Stephan Boyer <stephan@stephanboyer.com>
2016-09-10 08:05:37 -0700

    Use Docker to make development easier

--- scripts/get-compiler.sh
+++ scripts/get-compiler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# This script tries to find a version of Clang >= 3.1 or GCC >= 4.9.
+# This script tries to find a version of Clang >= 3.1 or GCC >= 4.7.
 
 # Usage:
 #   ./get-compiler.sh CC
@@ -13,8 +13,8 @@ if ! echo "$1" | grep -qi 'CC\|CXX'; then
 fi
 
 # Clang >= 3.1
-if (clang --version 2> /dev/null | grep -qi ' 3\.[1-9]') &&
-  clang++ --version 2> /dev/null | grep -qi ' 3\.[1-9]'; then
+if (clang --version 2> /dev/null | grep -qi ' \(3\.[1-9]\|[4-9]\.\)') &&
+  clang++ --version 2> /dev/null | grep -qi ' \(3\.[1-9]\|[4-9]\.\)'; then
   if echo "$1" | grep -qi 'CC'; then echo clang; exit; fi
   if echo "$1" | grep -qi 'CXX'; then echo clang++; exit; fi
 fi
@@ -26,19 +26,13 @@ if (clang --version 2> /dev/null | grep -qi 'apple llvm version [5-9]\.') &&
   if echo "$1" | grep -qi 'CXX'; then echo clang++; exit; fi
 fi
 
-# GCC >= 4.9
-if (gcc --version 2> /dev/null | grep -qi ' \(4\.9\.\)\|\([5-9]\.\)') &&
-  (gcc++ --version 2> /dev/null | grep -qi ' \(4\.9\.\)\|\([5-9]\.\)'); then
+# GCC >= 4.7
+if (gcc --version 2> /dev/null | grep -qi ' \(4\.7\.\|[5-9]\.\)') &&
+  (g++ --version 2> /dev/null | grep -qi ' \(4\.7\.\|[5-9]\.\)'); then
   if echo "$1" | grep -qi 'CC'; then echo gcc; exit; fi
   if echo "$1" | grep -qi 'CXX'; then echo g++; exit; fi
 fi
 
-# GCC = 4.9, sometimes installed by the install-deps.sh script
-if which gcc-4.9 >/dev/null 2>&1 && which g++-4.9 >/dev/null 2>&1; then
-  if echo "$1" | grep -qi 'CC'; then echo gcc-4.9; exit; fi
-  if echo "$1" | grep -qi 'CXX'; then echo g++-4.9; exit; fi
-fi
-
 # If we made it this far, we were unable to determine the compiler.
-echo 'No suitable compiler found. You must have Clang >= 3.1 or GCC >= 4.9.' >&2
+echo 'No suitable compiler found. You must have Clang >= 3.1 or GCC >= 4.7.' >&2
 exit 1
