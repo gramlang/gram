dfe2313d3dcc670450e61224fb43487f14d1acb1
Stephan Boyer <stephan@stephanboyer.com>
2016-11-28 00:51:59 -0800

    Refactor to use raw indices rather than line/col numbers

--- src/error.cpp
+++ src/error.cpp
@@ -6,30 +6,63 @@ gram::Error::Error(std::string message) : message(message) {
 gram::Error::Error(
   std::string message,
   const std::string &source, std::string source_name,
-  size_t start_line, size_t start_col,
-  size_t end_line, size_t end_col
+  size_t start_pos, size_t end_pos
 ) {
-  this->message = source_name +
-    ":" + std::to_string(start_line + 1) +
-    ":" + std::to_string(start_col + 1) + "\n" + message;
-
-  // Try to find the line(s) from the source by the line numbers.
+  // Compute line numbers, column numbers, and context boundaries.
+  size_t start_line = 0;
+  size_t start_col = 0;
+  size_t end_line = 0;
+  size_t end_col = 0;
+  size_t context_start_pos = 0;
+  size_t context_end_pos = source.size();
   size_t line_number = 0;
-  size_t start_pos = 0;
-  size_t end_pos = source.size();
-  for (size_t pos = 0; pos < source.size(); ++pos) {
-    if (source[pos] == '\n') {
+  size_t col_number = 0;
+  bool found_start = false;
+  bool found_end = false;
+  for (size_t pos = 0; pos <= source.size(); ++pos) {
+    if (pos == start_pos) {
+      start_line = line_number;
+      start_col = col_number;
+      found_start = true;
+    }
+    if (pos == end_pos) {
+      end_line = line_number;
+      end_col = col_number;
+      found_end = true;
+    }
+    if (pos == source.size() || source[pos] == '\n') {
       ++line_number;
-      if (line_number == start_line) {
-        start_pos = pos + 1;
+      col_number = 0;
+      if (!found_start) {
+        context_start_pos = pos + 1;
       }
-      if (line_number == end_line + 1) {
-        end_pos = pos;
+      if (found_end) {
+        context_end_pos = pos;
         break;
       }
+    } else {
+      ++col_number;
     }
   }
-  auto context = source.substr(start_pos, end_pos - start_pos);
+  auto context = source.substr(
+    context_start_pos,
+    context_end_pos - context_start_pos
+  );
+
+  // Output the source name, position, and message.
+  if (end_pos == start_pos || end_pos == start_pos + 1) {
+    this->message = source_name +
+      " @ " + std::to_string(start_line + 1) +
+      ":" + std::to_string(start_col + 1) +
+      "\n" + message;
+  } else {
+    this->message = source_name +
+      " @ " + std::to_string(start_line + 1) +
+      ":" + std::to_string(start_col + 1) +
+      " - " + std::to_string(end_line + 1) +
+      ":" + std::to_string(end_col) +
+      "\n" + message;
+  }
 
   // Check if the context has only whitespace.
   auto only_whitespace = true;
--- src/error.h
+++ src/error.h
@@ -9,13 +9,6 @@
 
 namespace gram {
 
-  // Notes about line and column numbering (also noted in lexer.h):
-  // - Line feeds exist on the lines they are terminating,
-  //   not the following line.
-  // - All indices must point to a valid character, with one exception:
-  //   the end of the file is represented by the last column number + 1,
-  //   with no change to the line number.
-
   class Error {
   private:
     std::string message;
@@ -23,12 +16,13 @@ namespace gram {
   public:
     explicit Error(std::string message);
     explicit Error(
-      std::string message,
-      const std::string &source, std::string source_name,
-      size_t start_line, size_t start_col, // Zero-indexed, inclusive
-      size_t end_line, size_t end_col // Zero-indexed, exclusive
+      std::string message, // No trailing line break
+      const std::string &source,
+      std::string source_name,
+      size_t start_pos, // Inclusive
+      size_t end_pos // Exclusive
     );
-    std::string what();
+    std::string what(); // No trailing line break
   };
 
 }
