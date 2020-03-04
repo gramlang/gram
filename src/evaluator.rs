28739ed7e7c4b49254bdd2639bf94ce5e5aa11f1
Stephan Boyer <stephan.boyer@airbnb.com>
2020-03-04 06:23:38 -0800

    Simplify the evaluator and fix a bug in it regarding nested lets

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -8,7 +8,7 @@ use crate::{
         },
     },
 };
-use std::{collections::HashSet, rc::Rc};
+use std::rc::Rc;
 
 // This function evaluates a term using a call-by-value strategy. The term is assumed to be
 // well-typed in the empty context. Runtime type errors will result in panicking.
@@ -77,80 +77,8 @@ fn step<'a>(term: &Rc<Term<'a>>) -> Option<Rc<Term<'a>>> {
             let mut substituted_definitions = definitions.clone();
             let mut substituted_body = body.clone();
             for i in 0..definitions.len() {
-                // Compute these once rather than multiple times.
+                // Compute this once rather than multiple times.
                 let i_index = definitions.len() - 1 - i;
-                let i_index_plus_1 = i_index + 1;
-
-                // Unfold each remaining definition and substitute it in.
-                for j in i..definitions.len() {
-                    // Compute this once rather than multiple times.
-                    let j_index = definitions.len() - 1 - j;
-
-                    // Determine which definitions are actually needed.
-                    let mut live_definitions = HashSet::new();
-                    get_live_definitions(
-                        &substituted_definitions,
-                        &Rc::new(Term {
-                            source_range: None,
-                            variant: Variable(substituted_definitions[j].0, 0),
-                        }),
-                        substituted_definitions.len(),
-                        &mut live_definitions,
-                    );
-
-                    // Unfold the definition.
-                    let unfolded_definition = Rc::new(Term {
-                        source_range: None,
-                        variant: Let(
-                            substituted_definitions[i..]
-                                .iter()
-                                .enumerate()
-                                .map(|(k, (variable, annotation, definition))| {
-                                    if live_definitions.contains(&(i + k)) {
-                                        (
-                                            *variable,
-                                            annotation.as_ref().map(|annotation| {
-                                                shift(
-                                                    annotation.clone(),
-                                                    definitions.len(),
-                                                    i_index_plus_1,
-                                                )
-                                            }),
-                                            shift(
-                                                definition.clone(),
-                                                definitions.len(),
-                                                i_index_plus_1,
-                                            ),
-                                        )
-                                    } else {
-                                        // The definition isn't needed. Replace it with an
-                                        // arbitrary value to avoid wasting (potentially infinite)
-                                        // time evaluating it.
-                                        (
-                                            *variable,
-                                            None,
-                                            Rc::new(Term {
-                                                source_range: None,
-                                                variant: Type,
-                                            }),
-                                        )
-                                    }
-                                })
-                                .collect(),
-                            Rc::new(Term {
-                                source_range: None,
-                                variant: Variable(substituted_definitions[j].0, 0),
-                            }),
-                        ),
-                    });
-
-                    // Substitute the unfolded definition.
-                    substituted_definitions[i].2 = open(
-                        substituted_definitions[i].2.clone(),
-                        j_index,
-                        unfolded_definition,
-                    );
-                }
 
                 // Try to step the definition.
                 if let Some(stepped_definition) = step(&substituted_definitions[i].2) {
@@ -173,21 +101,55 @@ fn step<'a>(term: &Rc<Term<'a>>) -> Option<Rc<Term<'a>>> {
                     }));
                 };
 
-                // Here is the fully stepped definition.
-                let stepped_definition = substituted_definitions[i].2.clone();
+                // Here is the fully stepped definition. Note that it may have free variables
+                // referencing itself or subsequent definitions.
+                let definition = substituted_definitions[i].2.clone();
 
                 // Ensure the definition is a value.
-                if !is_value(&stepped_definition) {
+                if !is_value(&definition) {
                     return None;
                 }
 
+                // Compute this once rather than multiple times.
+                let body_for_unfolding = Rc::new(Term {
+                    source_range: None,
+                    variant: Variable(substituted_definitions[i].0, 0),
+                });
+
+                // Unfold the definition.
+                let unfolded_definition = open(
+                    definition.clone(),
+                    i_index,
+                    Rc::new(Term {
+                        source_range: None,
+                        variant: Let(
+                            vec![(
+                                substituted_definitions[i].0,
+                                substituted_definitions[i].1.as_ref().map(|annotation| {
+                                    open(
+                                        shift(annotation.clone(), 0, 1),
+                                        i_index + 1,
+                                        body_for_unfolding.clone(),
+                                    )
+                                }),
+                                open(
+                                    shift(definition, 0, 1),
+                                    i_index + 1,
+                                    body_for_unfolding.clone(),
+                                ),
+                            )],
+                            body_for_unfolding,
+                        ),
+                    }),
+                );
+
                 // Substitute the value in subsequent definitions.
                 for definition in substituted_definitions.iter_mut().skip(i + 1) {
-                    definition.2 = open(definition.2.clone(), i_index, stepped_definition.clone());
+                    definition.2 = open(definition.2.clone(), i_index, unfolded_definition.clone());
                 }
 
                 // Substitute the value in the body.
-                substituted_body = open(substituted_body.clone(), i_index, stepped_definition);
+                substituted_body = open(substituted_body.clone(), i_index, unfolded_definition);
             }
 
             // Try to step the body.
@@ -297,85 +259,6 @@ fn is_value<'a>(term: &Term<'a>) -> bool {
     match term.variant {
         Type | Lambda(_, _, _) | Pi(_, _, _) | Integer | IntegerLiteral(_) => true,
         Variable(_, _) | Application(_, _) | Let(_, _) | Sum(_, _) | Difference(_, _) => false,
-    }
-}
-
-// Given a set of definitions and a term, this function determines which of the definitions are
-// actually needed to evaluate the term.
-#[allow(clippy::type_complexity)]
-fn get_live_definitions<'a>(
-    definitions: &[(&'a str, Option<Rc<Term<'a>>>, Rc<Term<'a>>)],
-    term: &Rc<Term<'a>>,
-    term_depth: usize,
-    live_definitions: &mut HashSet<usize>,
-) {
-    match &term.variant {
-        Type | Pi(_, _, _) | Integer | IntegerLiteral(_) => {
-            // Note that we ignore the contents of pi types since they are not needed for
-            // evaluation.
-        }
-        Variable(_, index) => {
-            // Compute this once rather than multiple times.
-            let definition_position = term_depth - 1 - index;
-
-            // Check if the variable points to one of the definitions.
-            if definition_position < definitions.len() {
-                // Check and update `live_definitions`.
-                if live_definitions.insert(definition_position) {
-                    // Recurse on the definition of the variable.
-                    get_live_definitions(
-                        definitions,
-                        &definitions[definition_position].2,
-                        definitions.len(),
-                        live_definitions,
-                    );
-                }
-            }
-        }
-        Lambda(_, _, body) => {
-            // Recurse on the body.
-            get_live_definitions(definitions, body, definitions.len() + 1, live_definitions);
-        }
-        Application(applicand, argument) => {
-            // Recurse on the applicand.
-            get_live_definitions(definitions, applicand, definitions.len(), live_definitions);
-
-            // Recurse on the argument.
-            get_live_definitions(definitions, argument, definitions.len(), live_definitions);
-        }
-        Let(let_definitions, body) => {
-            // Recurse on the definitions.
-            for (_, _, definition) in let_definitions {
-                get_live_definitions(
-                    definitions,
-                    definition,
-                    definitions.len() + let_definitions.len(),
-                    live_definitions,
-                );
-            }
-
-            // Recurse on the body.
-            get_live_definitions(
-                definitions,
-                body,
-                definitions.len() + let_definitions.len(),
-                live_definitions,
-            );
-        }
-        Sum(summand1, summand2) => {
-            // Recurse on the left summand.
-            get_live_definitions(definitions, summand1, definitions.len(), live_definitions);
-
-            // Recurse on the right summand.
-            get_live_definitions(definitions, summand2, definitions.len(), live_definitions);
-        }
-        Difference(minuend, subtrahend) => {
-            // Recurse on the minuend.
-            get_live_definitions(definitions, minuend, definitions.len(), live_definitions);
-
-            // Recurse on the subtrahend.
-            get_live_definitions(definitions, subtrahend, definitions.len(), live_definitions);
-        }
     }
 }
 
