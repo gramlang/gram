3f6738a3eb6ffa3ddac5659ae0afa345878fba3b
Stephan Boyer <stephan@stephanboyer.com>
2026-07-23 05:36:22 -0700

    Update dependencies

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,5 @@
 use crate::format::CodeStr;
 use colored::{Colorize, control::SHOULD_COLORIZE};
-use pad::{Alignment, PadStr};
 use std::{
     cmp::{max, min},
     error, fmt,
@@ -162,10 +161,7 @@ pub fn listing(source_contents: &str, source_range: SourceRange) -> String {
         .map(|(i, (line_number, line, section_start, section_end))| {
             format!(
                 "{}{}{}{}{}",
-                format!(
-                    "{} \u{2502} ",
-                    line_number.pad(gutter_width, ' ', Alignment::Right, false),
-                )
+                format!("{line_number:>gutter_width$} \u{2502} ")
                 .blue()
                 .bold(),
                 &line[..*section_start],
